a5edadf120fad0e7382a5c41fefef7bc67f3743d
youssef El fenary <145854563+y0ussefel@users.noreply.github.com>
2024-11-27 23:47:56 +0100

    Update Tr.py

--- python-fund/Tr.py
+++ python-fund/Tr.py
@@ -1,2 +1,13 @@
 for i in range(1, 10+1):
     print("*" * i)
+
+print("######################################")
+
+
+x = 7
+y = 15
+
+while x < y:
+    print("ana 7ma9 | x = " ,x )  
+    x += 1 
+print("Safi nta b3a9lek | x=y" )
